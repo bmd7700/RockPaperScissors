9575f15c10fa02346ed4cc99e85761a1d68680da
bmd7700 <bendrt@gmail.com>
2025-02-25 09:42:07 -0500

    add scoreboard

--- index.js
+++ index.js
@@ -25,34 +25,42 @@ function getComputerChoice(){
 }
 
     function playRound(humanChoice, computerChoice){
+        let computerScore = 0;
+        let humanScore = 0;
         if(humanChoice === computerChoice){
             console.log('It\'s a tie! Try again.');
         }
         else if(humanChoice === 'paper'){
             if(computerChoice === 'rock'){
                 console.log('Paper beats rock! +1 for humanity.');
-                //humanScore++;
+                humanScore++;
             } else if(computerChoice === 'scissors') {
                 console.log('Scissors beats paper! +1 for the machines.')
-                //computerScore++;
+                computerScore++;
             }
         }
         else if(humanChoice === 'scissors'){
             if(computerChoice === 'paper'){
                 console.log('Scissors beats paper! +1 for humans.');
-                //humanScore++;
+                humanScore++;
             } else if(computerChoice === 'rock'){
                 console.log('Rock smashes scissors. Bummer.');
-                //computerScore++;
+                computerScore++;
             }
         }
         else if(humanChoice === 'rock'){
             if(computerChoice === 'paper'){
                 console.log('Paper smothers rock. One more for the machines.');
-                //computerScore++;
+                computerScore++;
             } else if(computerChoice === 'scissors'){
                 console.log('Rock smashes scissors. Humanity triumphs!');
-                //humanScore++;
+                humanScore++;
             }
         }
+        //scoreboard
+        let humanCounter = document.querySelector("#human");
+        humanCounter.textContent = humanScore.toString();
+        let computerCounter = document.querySelector("#computer");
+        computerCounter.textContent = computerScore.toString();
+
     }
